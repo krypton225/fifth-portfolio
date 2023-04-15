ce8958dae53660cc1f8e1484ed9c649975392833
krypton225 <k.mohamed.11.98@gmail.com>
2023-04-15 10:49:26 +0200

    ADD Navbar component in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Navbar } from "components";
+
 function App() {
-    return <div className="hegazi-app">App</div>;
+    return (
+        <div className="hegazi-app">
+            <Navbar />
+        </div>
+    );
 }
 
 export default App;
