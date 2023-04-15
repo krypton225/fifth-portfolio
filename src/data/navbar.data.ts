093fbf3098db07893b462d7bfcc05461a1ecc60d
krypton225 <k.mohamed.11.98@gmail.com>
2023-04-15 10:46:54 +0200

    ADD navbar data of links

---
+++ src/data/navbar.data.ts
@@ -0,0 +1,15 @@
+import GenerateId from "utils/generate-id";
+
+import INavbar from "components/Navbar/Navbar.interface";
+
+const NavbarData: Array<INavbar> = [
+    { url: "#", textLink: "home", isButton: false },
+    { url: "#", textLink: "projects", isButton: false },
+    { url: "#", textLink: "blog", isButton: false },
+    { url: "#", textLink: "certificates", isButton: false },
+    { url: "#", textLink: "hire me", isButton: true },
+];
+
+(() => GenerateId(NavbarData))();
+
+export default NavbarData;
