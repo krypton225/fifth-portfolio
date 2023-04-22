18624bcb4b6f4722dc70032bcc4064743241fc2d
krypton225 <k.mohamed.11.98@gmail.com>
2023-04-22 18:26:10 +0200

    UPD urls in navbar data

--- src/data/navbar.data.ts
+++ src/data/navbar.data.ts
@@ -3,12 +3,12 @@ import GenerateId from "utils/generate-id";
 import INavbar from "components/Navbar/Navbar.interface";
 
 const NavbarData: Array<INavbar> = [
-    { url: "#", textLink: "home", isButton: false },
-    { url: "#", textLink: "projects", isButton: false },
-    { url: "#", textLink: "blog", isButton: false },
-    { url: "#", textLink: "certificates", isButton: false },
-    { url: "#", textLink: "contact me", isButton: false },
-    { url: "#", textLink: "hire me", isButton: true },
+    { url: "home", textLink: "home", isButton: false },
+    { url: "projects", textLink: "projects", isButton: false },
+    { url: "blog", textLink: "blog", isButton: false },
+    { url: "certificates", textLink: "certificates", isButton: false },
+    { url: "contact-me", textLink: "contact me", isButton: false },
+    { url: "hire-me", textLink: "hire me", isButton: true },
 ];
 
 (() => GenerateId(NavbarData))();
