e1e14c32bab1611137788a9470e3e35a9036a568
krypton225 <k.mohamed.11.98@gmail.com>
2023-04-23 17:09:52 +0200

    ADD new logo images

--- src/data/images.ts
+++ src/data/images.ts
@@ -1,3 +1,7 @@
 export { default as PersonalPicture } from "assets/images/Personal-Pic.svg";
 
 export { default as CourseraLogo } from "assets/images/coursera.svg";
+
+export { default as ITILogo } from "assets/images/iti-logo.svg";
+
+export { default as IPICLogo } from "assets/images/ipic-logo.svg";
