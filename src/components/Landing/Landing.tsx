7d40f32ec37f4d3e6f1e38a65f60c35daedf53a1
krypton225 <k.mohamed.11.98@gmail.com>
2023-04-25 18:19:19 +0200

    ADD links for buttons in Landing component

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -1,6 +1,5 @@
-import Button from "components/Button/Button";
-
 import { PersonalPicture } from "data/images";
+import { Link } from "react-scroll";
 
 const Landing = () => {
     return (
@@ -9,17 +8,40 @@ const Landing = () => {
                 <div className="w-full flex justify-center lg:justify-between items-center flex-wrap gap-5 sm:gap-10">
                     <div className="text-center sm:text-start chn-text-mq">
                         <p className="text-lg font-light">Hi I&apos;m,</p>
+
                         <p className="my-2 capitalize text-4xl md:text-6xl font-semibold">
                             khaled hegazi
                         </p>
+
                         <p className="capitalize text-lg md:text-3xl text-gray-color">
                             UI/UX designer
                         </p>
+
                         <div className="w-full md:w-[75%] relative mt-4 flex justify-center md:justify-between items-center flex-wrap gap-5">
-                            <Button text="hire me" url="#" hasBg />
-                            <Button text="projects" url="#" hasBg={false} />
+                            {/* <Button text="hire me" url="#" hasBg /> */}
+                            {/* <Button text="projects" url="projects" hasBg={false} /> */}
+                            <Link
+                                to="contact-me"
+                                smooth={true}
+                                offset={-70}
+                                duration={900}
+                                delay={200}
+                                className="btn btn-has-bg">
+                                hire me
+                            </Link>
+
+                            <Link
+                                to="projects"
+                                className="btn btn-has-bg"
+                                smooth={true}
+                                offset={-70}
+                                duration={900}
+                                delay={200}>
+                                projects
+                            </Link>
                         </div>
                     </div>
+
                     <div
                         className={`w-[14rem] sm:w-[16rem] lg:w-[22rem] h-[14rem] sm:h-[16rem] lg:h-[22rem]`}>
                         <img
