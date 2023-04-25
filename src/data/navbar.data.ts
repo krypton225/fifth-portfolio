d56040eca2ec5d0a7f6200d4ff294a047946d3be
krypton225 <k.mohamed.11.98@gmail.com>
2023-04-25 15:18:17 +0200

    REM blog link from navbar data

--- src/data/navbar.data.ts
+++ src/data/navbar.data.ts
@@ -5,7 +5,6 @@ import INavbar from "components/Navbar/Navbar.interface";
 const NavbarData: Array<INavbar> = [
     { url: "home", textLink: "home", isButton: false },
     { url: "projects", textLink: "projects", isButton: false },
-    { url: "blog", textLink: "blog", isButton: false },
     { url: "certificates", textLink: "certificates", isButton: false },
     { url: "contact-me", textLink: "contact me", isButton: false },
     { url: "hire-me", textLink: "hire me", isButton: true },
