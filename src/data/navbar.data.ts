17fdb6db9c7ce7e7e41f02f1f7bcb1e343e1eb73
krypton225 <k.mohamed.11.98@gmail.com>
2023-04-25 17:51:04 +0200

    UPD navbar link for hire me

--- src/data/navbar.data.ts
+++ src/data/navbar.data.ts
@@ -7,7 +7,7 @@ const NavbarData: Array<INavbar> = [
     { url: "projects", textLink: "projects", isButton: false },
     { url: "certificates", textLink: "certificates", isButton: false },
     { url: "contact-me", textLink: "contact me", isButton: false },
-    { url: "hire-me", textLink: "hire me", isButton: true },
+    { url: "contact-me", textLink: "hire me", isButton: true },
 ];
 
 (() => GenerateId(NavbarData))();
