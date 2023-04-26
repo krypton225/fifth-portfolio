3eb03cc93318362967fc3303ddda5b2e3af78139
krypton225 <k.mohamed.11.98@gmail.com>
2023-04-26 19:22:56 +0200

    UPD formatting in Certificates component

--- src/components/Certificates/Certificates.tsx
+++ src/components/Certificates/Certificates.tsx
@@ -23,50 +23,51 @@ const Certificates = () => {
 
             <div className="container">
                 <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-14 gap-y-10">
-                    {/* shadow-blue-500 shadow-lg */}
-                    {CertificatesData.map(({ id, name, company, date, url, delayAnimation }) => (
-                        <div
-                            key={id}
-                            className={`w-full min-h-[14rem] py-6 relative 
+                    {
+                        CertificatesData.map(({ id, name, company, date, url, delayAnimation }) => (
+                            <div
+                                key={id}
+                                className={`w-full min-h-[14rem] py-6 relative 
                                 border-zinc-200 border-l-primary border-solid border-[2px] border-l-[6px] 
                                 rounded-md flex flex-col justify-between items-center
                                 text-primary text-center shadow-gray-300 shadow-lg`}
-                            data-aos="fade-up"
-                            data-aos-offset="100"
-                            data-aos-easing="ease-in-out"
-                            data-aos-duration="1000"
-                            data-aos-delay={`${delayAnimation}`}>
-                            <p className="px-4 text-base font-semibold capitalize leading-5">
-                                {name}
-                            </p>
+                                data-aos="fade-up"
+                                data-aos-offset="100"
+                                data-aos-easing="ease-in-out"
+                                data-aos-duration="1000"
+                                data-aos-delay={`${delayAnimation}`}>
+                                <p className="px-4 text-base font-semibold capitalize leading-5">
+                                    {name}
+                                </p>
 
-                            <div className="w-24 mt-4 mb-2 relative">
-                                <a href={company.link} target="_blank" rel="noreferrer">
-                                    <img
-                                        src={company.logo}
-                                        alt={company.altText}
-                                        className="w-full object-cover"
-                                        draggable="false"
-                                        title={company.altText}
-                                    />
-                                </a>
-                            </div>
+                                <div className="w-24 mt-4 mb-2 relative">
+                                    <a href={company.link} target="_blank" rel="noreferrer">
+                                        <img
+                                            src={company.logo}
+                                            alt={company.altText}
+                                            className="w-full object-cover"
+                                            draggable="false"
+                                            title={company.altText}
+                                        />
+                                    </a>
+                                </div>
 
-                            <p className="mb-3">{`${date}`}</p>
+                                <p className="mb-3">{`${date}`}</p>
 
-                            <p className="w-full">
-                                <a
-                                    className={`w-[80%] rounded-md py-1 inline-block bg-primary text-white 
+                                <p className="w-full">
+                                    <a
+                                        className={`w-[80%] rounded-md py-1 inline-block bg-primary text-white 
                                         transition-colors duration-700 hover:bg-primary-2`}
-                                    href={`${url}`}
-                                    target="_blank"
-                                    rel="noreferrer"
-                                    title={`Show ${name} certificate`}>
-                                    Show certificate
-                                </a>
-                            </p>
-                        </div>
-                    ))}
+                                        href={`${url}`}
+                                        target="_blank"
+                                        rel="noreferrer"
+                                        title={`Show ${name} certificate`}>
+                                        Show certificate
+                                    </a>
+                                </p>
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
         </section>
