cf83b0f3bd2f7bc5cf6c44fafdda541500151197
krypton225 <k.mohamed.11.98@gmail.com>
2023-04-26 18:26:57 +0200

    ADD index file in utils dir & REM one @ interfaces

---
+++ src/utils/index.ts
@@ -0,0 +1,7 @@
+export { default as Delay } from "utils/generate-delay";
+
+export { default as GenerateID } from "utils/generate-id";
+
+export type { default as IDelayAnimation } from "utils/interfaces/IDelayAnimation.interface";
+
+export type { default as ISetID } from "utils/interfaces/ISetID.interface";
--- src/utils/interfaces/index.ts
+++
@@ -1,3 +0,0 @@
-export type { default as IDelayAnimation } from "./IDelayAnimation.interface";
-
-export type { default as ISetID } from "./ISetID.interface";
