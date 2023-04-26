86e38215960ca36afc8f00972a5663a4c4fb034b
krypton225 <k.mohamed.11.98@gmail.com>
2023-04-26 13:31:40 +0200

    ADD AOS for Projects component

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,4 +1,8 @@
-import { useState } from "react";
+/* eslint-disable prettier/prettier */
+
+import { useState, useEffect } from "react";
+
+import AOS from "aos";
 
 import Button from "components/Button/Button";
 
@@ -9,29 +13,44 @@ import ProjectsData from "data/projects.data";
 const Projects = () => {
     const [numberOfShowedProjects] = useState(6);
 
+    useEffect(() => {
+        AOS.init({
+            once: true,
+        });
+
+        AOS.refresh();
+    }, []);
+
     return (
         <section className="w-full" id="projects">
             <SectionTitle text="projects" />
-
             <div className="container grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-14 gap-y-10">
-                {ProjectsData.slice(0, numberOfShowedProjects).map(({ id, name, image, url }) => (
-                    <div key={id} className="min-h-[17rem] relative rounded-lg group">
-                        <img
-                            src={image}
-                            alt={url.altText}
-                            className="w-full h-full rounded-lg object-cover"
-                        />
+                {ProjectsData.slice(0, numberOfShowedProjects)
+                        .map(({ id, name, image, url, delayAnimation }) => (
+                            <div
+                                key={id}
+                                className="min-h-[17rem] relative rounded-lg group"
+                                data-aos="fade-up"
+                                data-aos-offset="100"
+                                data-aos-easing="ease-in-out"
+                                data-aos-duration="800"
+                                data-aos-delay={`${delayAnimation}`}>
+                                <img
+                                    src={image}
+                                    alt={url.altText}
+                                    className="w-full h-full rounded-lg object-cover"
+                                />
 
-                        <div
-                            className={`min-w-full min-h-full rounded-lg py-3 px-2 absolute top-0 left-0 invisible opacity-0
-                        flex flex-col justify-center items-center overlay-project transition-all duration-700
-                        group-hover:visible group-hover:opacity-100`}>
-                            <p className="mb-7 capitalize text-white text-md">{name}</p>
+                                <div
+                                    className={`min-w-full min-h-full rounded-lg py-3 px-2 absolute top-0 left-0 invisible opacity-0 
+                                    flex flex-col justify-center items-center overlay-project transition-all duration-700 
+                                    group-hover:visible group-hover:opacity-100`}>
+                                    <p className="mb-7 capitalize text-white text-md">{name}</p>
 
-                            <Button text="view" hasBg hasBgOnHover url={url.src} />
-                        </div>
-                    </div>
-                ))}
+                                    <Button text="view" hasBg hasBgOnHover url={url.src} />
+                                </div>
+                            </div>
+                        ))}
             </div>
 
             <div className="w-full mt-14 flex justify-center items-center">
