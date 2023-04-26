144d7a084c23be22195ac111e9172cf995ab7f98
krypton225 <k.mohamed.11.98@gmail.com>
2023-04-26 12:35:20 +0200

    ADD tooltip for links @ SocialMedia component

--- src/components/SocialMedia/SocialMedia.tsx
+++ src/components/SocialMedia/SocialMedia.tsx
@@ -18,7 +18,7 @@ const SocialMedia = () => {
     return (
         <div
             // lg:w-fit lg:py-3 lg:rounded-xl
-            className={`max-[450px]:w-[65%] min-[451px]:w-[45%] max-[639px]:w-[45%] sm:w-[45%] md:w-[18rem] h-12 overflow-hidden
+            className={`max-[450px]:w-[65%] min-[451px]:w-[45%] max-[639px]:w-[45%] sm:w-[45%] md:w-[18rem] h-12 
             ${isReachToPoint ? "bottom-[4%]" : "-bottom-[15%]"} 
             rounded-full fixed left-[35%] md:left-1/2 z-[999999999] -translate-x-[45%] md:-translate-x-1/2 
             flex justify-between items-center bg-primary transition-all duration-[1s]`}>
@@ -27,7 +27,19 @@ const SocialMedia = () => {
                     key={id}
                     className={`w-7 lg:w-8 h-7 lg:h-8 relative rounded-full mx-2 lg:mx-4 
                     flex justify-center items-center text-primary bg-white`}>
-                    <a href={src} title={altText} target="_blank" rel="noreferrer">
+                    <a
+                        href={src}
+                        data-current-title={altText}
+                        target="_blank"
+                        rel="noreferrer"
+                        className={`w-full h-full relative flex justify-center items-center
+                        
+                        md:before:content-[attr(data-current-title)] md:before:w-fit md:before:py-2 md:before:px-3 
+                        md:before:rounded-md md:before:absolute md:before:top-[-170%] md:before:-left-[50%] 
+                        md:before:capitalize md:before:text-sm md:before:bg-primary md:before:text-white
+                        md:before:invisible md:before:opacity-0 before:transition-all before:duration-300
+                        
+                        hover:before:visible hover:before:opacity-100 hover:before:top-[-190%]`}>
                         <i className={`icon ${icon}`}></i>
                     </a>
                 </div>
