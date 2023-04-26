14e9f30e22c69ce3f2cd8e0e3c1bcb9ff99a4751
krypton225 <k.mohamed.11.98@gmail.com>
2023-04-26 10:52:52 +0200

    ADD styles for SectionTitle component

--- src/components/SectionTitle/SectionTitle.tsx
+++ src/components/SectionTitle/SectionTitle.tsx
@@ -3,7 +3,14 @@ import ISectionTitle from "./ISectionTitle.interface";
 const SectionTitle = ({ text }: ISectionTitle) => {
     return (
         <h2
-            className={`w-full mb-8 md:mb-12 relative font-[500] text-3xl lg:text-4xl capitalize text-center`}>
+            className={`w-fit mx-auto mb-8 md:mb-12 relative font-[500] text-3xl lg:text-4xl capitalize text-center
+            before:content-[''] before:w-3 before:h-3 before:rounded-full 
+            before:absolute before:top-1/2 before:-translate-y-1/2 before:-left-[22%]
+            before:bg-primary
+            
+            after:content-[''] after:w-3 after:h-3 after:rounded-full 
+            after:absolute after:top-1/2 after:-translate-y-1/2 after:-right-[22%]
+            after:bg-primary`}>
             {text}
         </h2>
     );
