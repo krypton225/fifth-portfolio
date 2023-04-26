4fbaa88d711fb0b0bd0eaa693da9ca2a7a9e1d89
krypton225 <k.mohamed.11.98@gmail.com>
2023-04-26 14:00:45 +0200

    ADD generator module for delay animations

---
+++ src/utils/generate-delay.ts
@@ -0,0 +1,48 @@
+const Delay = (function () {
+    function* getNumberID(start?: number): Generator<number> {
+        let increase: number = start || 50;
+
+        while (true) {
+            yield (increase += 100);
+        }
+    }
+
+    /**
+     * @param  {Array} targetArray=[] - array to check if it has objects or not.
+     */
+    function isArrayNotEmpty(targetArray: Array<object> = []): boolean {
+        return targetArray.length !== 0;
+    }
+
+    /**
+     * @param  {Array} dataArr=[] - array has objects and insert delayAnimation into each one.
+     */
+    // prettier-ignore
+    function generateNumber(dataArr: Array<object> = []): void {
+        if (isArrayNotEmpty(dataArr)) {
+            const counter: Generator<number> = getNumberID();
+
+            dataArr.forEach((row) :void => {
+                if (typeof row === "object") {
+                    if (!row.hasOwnProperty("delayAnimation")) { // eslint-disable-line
+                        Object.defineProperty(row, "delayAnimation", {
+                            value: counter.next().value,
+                            writable: false,
+                            enumerable: true,
+                        });
+                    }
+                } else {
+                    throw new Error(`Array must contain only object.`);
+                }
+            });
+        } else {
+            throw new Error("Array must not be empty of objects.");
+        }
+    }
+
+    return {
+        generate: generateNumber,
+    };
+})();
+
+export default Delay;
