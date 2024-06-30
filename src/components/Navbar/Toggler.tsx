de6718a05a76e66cc43be36f4c471efd5d1206e1
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2024-06-30 12:15:23 +0300

    fix: :bug: fix `width` & `height` of `Toggler` component

--- src/components/Navbar/Toggler.tsx
+++ src/components/Navbar/Toggler.tsx
@@ -3,12 +3,12 @@ import IToggler from "./IToggler.interface";
 const Toggler = ({ togglerState, onClickTogglerState }: IToggler) => {
     return (
         <div
-            className={`min-w-[2.5rem] min-h-[1.3rem] lg:hidden 
+            className={`min-w-[2rem] min-h-[1.1rem] lg:hidden 
             flex flex-col justify-between items-center cursor-pointer transition-all duration-[1s]`}
             onClick={onClickTogglerState}>
             <span
                 className={`toggle-line duration-[0.7s] 
-                ${togglerState ? "-rotate-45 translate-y-[0.57rem]" : ""}`}></span>
+                ${togglerState ? "-rotate-45 translate-y-2" : ""}`}></span>
             <span
                 className={`toggle-line duration-[0.3s] 
             ${togglerState ? "translate-x-5 opacity-0" : ""}`}></span>
